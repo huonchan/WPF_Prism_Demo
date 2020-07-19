46b9580cda2e04306f86b767e5f81ac93521ba7d
huonchan <dmht1127@gmail.com>
2020-07-19 15:19:25 +0900

    ちんこ

---
+++ SimplePrismDemo/PrismModuleDemo/PrismModuleDemoModule.cs
@@ -0,0 +1,21 @@
+﻿using PrismModuleDemo.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace PrismModuleDemo
+{
+    public class PrismModuleDemoModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            var regionManager = containerProvider.Resolve<IRegionManager>();
+            regionManager.RegisterViewWithRegion("ContentRegion", typeof(ViewA));
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+    }
+}
---
+++ SimplePrismDemo/SimplePrismDemo/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Prism.Ioc;
+using SimplePrismDemo.Views;
+using System.Windows;
+using Prism.Modularity;
+using PrismModuleDemo;
+
+namespace SimplePrismDemo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<PrismModuleDemo.Views.ViewA>();
+        }
+
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+            moduleCatalog.AddModule<PrismModuleDemoModule>(InitializationMode.WhenAvailable);
+        }
+    }
+
+}
